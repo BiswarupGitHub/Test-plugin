c80d77d847f5fb3ba78e0cc9ee6029b85ee9c043
Biswarup Deb <biswarup.deb.18@gmail.com>
2022-11-08 22:19:00 +0530

    Add files via upload

--- k_neighbours/k_neighbours.py
+++ k_neighbours/k_neighbours.py
@@ -23,13 +23,125 @@
 """
 from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
 from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtWidgets import QAction
+from qgis.PyQt.QtWidgets import QAction, QFileDialog
 
 # Initialize Qt resources from file resources.py
 from .resources import *
 # Import the code for the dialog
 from .k_neighbours_dialog import kneighboursDialog
+from sklearn.linear_model import LogisticRegression
 import os.path
+
+import tensorflow as tf
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import pandas as pd
+import gzip
+
+#matplotlib inline
+ina = "E:\\Nesac_py\\plugin project\\pixel-data_60m\\pixel_data\\60m\\PNL_A018780_20201010_60m.csv"
+with open(ina, 'rb') as fd1:
+    gzip_fd1 = gzip.GzipFile(fileobj=fd1)
+    data1 = pd.read_csv(gzip_fd1)
+    #print(data1.iloc[0])
+    
+#Second satellite image
+inb = 'E:\\Nesac_py\\plugin project\\pixel-data_60m\\pixel_data\\60m\\PQM_A018880_20201017_60m.csv'
+with open(inb, 'rb') as fd2:
+    gzip_fd2 = gzip.GzipFile(fileobj=fd2)
+    data2 = pd.read_csv(gzip_fd2)
+    #print(data2.iloc[0])
+
+data = pd.concat([data1, data2], ignore_index=True)
+#print(data)
+
+xallpd = data.iloc[:, 2:]
+indpd = data.iloc[:,0]
+yallpd = data.iloc[:,1]
+
+xall = pd.DataFrame(xallpd).to_numpy()
+indall = pd.DataFrame(indpd).to_numpy()
+yall = pd.DataFrame(yallpd).to_numpy()
+
+yall[yall==1] = 0
+yall[yall==2] = 1
+
+# print(np.count_nonzero(yall==0))
+# print(np.count_nonzero(yall==1))
+
+inc = "E:\\Nesac_py\\plugin project\\pixel-data_60m\\pixel_data\\60m\\PNM_A027474_20200925T082711_60m.csv"
+with open(inc, 'rb') as fd3:
+    gzip_fd3 = gzip.GzipFile(fileobj=fd3)
+    data3 = pd.read_csv(gzip_fd3) 
+
+xallpd_test = data3.iloc[:, 2:]
+indpd_test = data3.iloc[:,0]
+yallpd_test = data3.iloc[:,1]
+#print(data3.iloc[0])
+
+xall_test = pd.DataFrame(xallpd_test).to_numpy()
+indall_test = pd.DataFrame(indpd_test).to_numpy()
+yall_test = pd.DataFrame(yallpd_test).to_numpy()
+
+yall_test[yall_test==1] = 0
+yall_test[yall_test==2] = 1
+
+
+#.....................................................................PRINCIPAL COMPONENT ANALYSIS FOR TEST.............................................................
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components=3)
+pcacomp = pca.fit_transform(xall)
+
+xall = pcacomp
+
+pca_test = PCA(n_components=3)
+pcacomp_test = pca_test.fit_transform(xall_test)
+
+xall_test = pcacomp_test
+
+from sklearn import preprocessing
+import random
+
+# Let's shuffle the data
+alltog1 = np.append(xall, indall, axis=1)
+alltog = np.append(alltog1, yall, axis=1)
+np.random.shuffle(alltog)
+
+xall = alltog[:,:-2]
+indall = alltog[:,-2]
+yall = alltog[:,-1]
+
+x_train = xall
+y_train = yall
+  
+scaler = preprocessing.StandardScaler().fit(x_train)
+x_train = scaler.transform(x_train)
+
+# Check prints
+# print(np.shape(x_train))
+# print(np.shape(y_train))
+
+# # Let's shuffle the data
+alltog1_test = np.append(xall_test, indall_test, axis=1)
+alltog_test = np.append(alltog1_test, yall_test, axis=1)
+np.random.shuffle(alltog_test)
+
+xall_test = alltog_test[:,:-2]
+indall_test = alltog_test[:,-2]
+yall_test = alltog_test[:,-1]
+
+x_test = xall_test
+y_test = yall_test
+
+scaler2 = preprocessing.StandardScaler().fit(x_test)
+x_test = scaler2.transform(x_test)
+
+# print(np.shape(x_test))
+# print(np.shape(y_test))
+
+from sklearn.neighbors import KNeighborsClassifier
 
 
 class kneighbours:
@@ -178,7 +290,16 @@ class kneighbours:
                 self.tr(u'&K Neighbours'),
                 action)
             self.iface.removeToolBarIcon(action)
+    
+    def select_output_file(self):
+      filename, _filter = QFileDialog.getSaveFileName(
+        self.dlg, "Select   output file ","", '*.txt')
+      self.dlg.lineEdit_4.setText(filename)
 
+    def select_output_file_a(self):
+      filename, _filter = QFileDialog.getSaveFileName(
+        self.dlg, "Select   output file ","", '*.txt')
+      self.dlg.lineEdit_5.setText(filename)
 
     def run(self):
         """Run method that performs all the real work"""
@@ -188,7 +309,8 @@ class kneighbours:
         if self.first_start == True:
             self.first_start = False
             self.dlg = kneighboursDialog()
-
+            self.dlg.pushButton_4.clicked.connect(self.select_output_file)
+            self.dlg.pushButton_5.clicked.connect(self.select_output_file_a)
         # show the dialog
         self.dlg.show()
         # Run the dialog event loop
@@ -197,4 +319,42 @@ class kneighbours:
         if result:
             # Do something useful here - delete the line containing pass and
             # substitute with your code.
-            pass
+            knn = KNeighborsClassifier(n_neighbors=8)
+            knn.fit(x_train,y_train.flatten())
+            knnpre = knn.predict(x_test)
+
+            y_knnpre = np.append(knnpre.reshape((np.shape(x_test)[0],1)), indall_test.reshape((np.shape(x_test)[0],1)), axis=1)
+
+            # # Check prints
+            # #print(knnpre[:10])
+            # #print(y_test.flatten()[:10])
+
+            # # Naive implementation for calculating prediction accuracy
+            # #knnsum = sum(x == y for x, y in zip(knnpre, y_test.flatten()))
+            # #knnacc = knnsum / float(len(knnpre))
+            # #print(knnacc)
+
+            # print(knn.score(x_train, y_train))
+            # print(knn.score(x_test, y_test))
+
+            # Let's save the obtained predictions into a txt file
+            np.savetxt(self.dlg.lineEdit_4.text(), y_knnpre, delimiter=',')
+
+            
+
+            # Try out different C values and see how it affects prediction accuracy!
+            logre = LogisticRegression(C=1e-6)
+            logre.fit(x_train, y_train.flatten())
+            logpre = logre.predict(x_test)
+
+            y_logpre = np.append(logpre.reshape((np.shape(x_test)[0],1)), indall_test.reshape((np.shape(x_test)[0],1)), axis=1)
+
+            # Check prints
+            #print(logpre[:10])
+            #print(y_test.flatten()[:10])
+
+            print(logre.score(x_train, y_train))
+            print(logre.score(x_test, y_test))
+
+            # Let's save the obtained predictions into a txt file
+            np.savetxt(self.dlg.lineEdit_5.text(), y_logpre, delimiter=',')
